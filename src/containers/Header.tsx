134d83088910150c491c666dbab8e97630db219c
Woogie-94 <keu1106@naver.com>
2021-10-01 06:39:16 +0900

    Done Header design

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import LogoImg from "../images/header_logo.png";
 
 const Header = (): JSX.Element => {
-  return <></>;
+  return (
+    <HeaderContainer>
+      <HeaderLogo>
+        <Link to="/">
+          <img src={LogoImg} alt="payhere" />
+        </Link>
+      </HeaderLogo>
+    </HeaderContainer>
+  );
 };
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 80px;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+`;
+
+const HeaderLogo = styled.h1`
+  width: 150px;
+
+  img {
+    width: 100%;
+  }
+`;
 
 export default Header;
