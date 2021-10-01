95e8703c0ce662da73a29f07d831e272bac65352
Woogie-94 <keu1106@naver.com>
2021-10-01 19:59:21 +0900

    Add button, input, textare reset

--- src/GlobalStyles.ts
+++ src/GlobalStyles.ts
@@ -3,11 +3,25 @@ import reset from "styled-reset";
 
 const GlobalStyles = createGlobalStyle`
   ${reset}
+  * {
+    box-sizing: border-box;
+  }
 
   a {
     text-decoration: none;
     color: inherit;
   }
+
+  button, input, textarea {
+    border: none;
+    outline: none;
+  }
+
+  button {
+    cursor: pointer;
+    background-color: inherit;
+  }
+
 
 `;
 
