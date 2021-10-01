3be5a4e00d8e275387dc24960fbd0f0470644d55
Woogie-94 <keu1106@naver.com>
2021-10-01 19:59:38 +0900

    Add Header, SearchSideBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Switch } from "react-router-dom";
+import Header from "./containers/Header";
+import SearchSideBar from "./containers/SearchSideBar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Header />
+      <SearchSideBar />
+      <Switch></Switch>
+    </>
+  );
 }
 
 export default App;
