8fa33e90e73f9808610ab1768c332133c5178242
Woogie-94 <keu1106@naver.com>
2021-10-02 17:03:58 +0900

    Add repos total count

--- src/containers/SearchSideBar.tsx
+++ src/containers/SearchSideBar.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
+import { ReposSelector } from "../reducer/repos";
+import { RepoReducerState, REQUEST_STATE } from "../types";
 import SearchForm from "./SearchForm";
 import SearchList from "./SearchList";
 
 const SearchSideBar = (): JSX.Element => {
+  const { state, total_count }: RepoReducerState = useSelector(ReposSelector);
+
   return (
     <BarContainer>
       <SearchForm />
+      <TotalCount>검색 결과: {state === REQUEST_STATE.SUCCESS ? total_count : "-"}</TotalCount>
       <SearchList />
     </BarContainer>
   );
@@ -21,6 +27,12 @@ const BarContainer = styled.div`
   padding: 50px 30px;
   background-color: #fff;
   border-left: 1px solid #d6d6d8;
+`;
+
+const TotalCount = styled.div`
+  font-size: 14px;
+  color: #777;
+  margin-bottom: 15px;
 `;
 
 export default SearchSideBar;
