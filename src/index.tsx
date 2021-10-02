94ba3d656024fd0d746d99a327080d6a29d5da18
Woogie-94 <keu1106@naver.com>
2021-10-02 17:01:31 +0900

    Add redux Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import GlobalStyles from "./GlobalStyles";
+import store from "./reducer/store";
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
