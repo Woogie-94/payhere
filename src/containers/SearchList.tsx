66edd0a102fbef86bf8b4a15239401e1da526de4
Woogie-94 <keu1106@naver.com>
2021-10-02 17:07:37 +0900

    Add made reactive to the data

--- src/containers/SearchList.tsx
+++ src/containers/SearchList.tsx
@@ -1,28 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
 import RepoCard from "../components/RepoCard";
+import { ReposSelector } from "../reducer/repos";
+import { RepoReducerState, REQUEST_STATE } from "../types";
+import Loading from "../components/Loading";
 
 const SearchList = (): JSX.Element => {
+  const { state, repos, empty }: RepoReducerState = useSelector(ReposSelector);
+
+  useEffect(() => {
+    if (empty) alert("검색 결과가 없습니다.");
+  }, [empty]);
+
   return (
     <ListContainer>
-      <RepoCard />
-      <RepoCard />
-      <RepoCard />
-      <RepoCard />
-      <RepoCard />
-      <RepoCard />
-      <RepoCard />
-      <RepoCard />
-      <RepoCard />
-      <RepoCard />
-      <RepoCard />
-      <RepoCard />
+      {state === REQUEST_STATE.INIT && <FailedMessage>검색을 이용해보세요</FailedMessage>}
+      {state === REQUEST_STATE.FAILED && <FailedMessage>잘못된 검색입니다.</FailedMessage>}
+      {state === REQUEST_STATE.PENDING && <Loading />}
+      {repos.map((repo) => (
+        <RepoCard key={repo.id} {...repo} />
+      ))}
     </ListContainer>
   );
 };
 
 const ListContainer = styled.div`
   overflow: overlay;
+  position: relative;
   width: 100%;
   height: calc(100% - 70px);
 
@@ -36,6 +41,17 @@ const ListContainer = styled.div`
   }
   &::-webkit-scrollbar-track {
   }
+`;
+
+const FailedMessage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  font-size: 20px;
+  font-weight: bold;
+  color: #aaa;
 `;
 
 export default SearchList;
