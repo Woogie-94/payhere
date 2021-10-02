bf7a821e40506d4748ebeb268823bd03a1c52806
Woogie-94 <keu1106@naver.com>
2021-10-02 17:19:20 +0900

    Add request getRepos with search value

--- src/containers/SearchForm.tsx
+++ src/containers/SearchForm.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import styled from "styled-components";
 import Input from "../components/Input";
+import useInput from "../hooks/useInput";
+import { getRepos } from "../reducer/repos";
 
 const SearchForm = (): JSX.Element => {
+  const [searchValue, onSerachValue] = useInput<string>("");
+  const dispatch = useDispatch();
+
+  const repo = useCallback(async (): Promise<void> => {
+    dispatch(getRepos({ searchValue }));
+  }, [searchValue, dispatch]);
+
+  useEffect(() => {}, [searchValue]);
+
   return (
     <FormContainer>
-      <Input styleType="searchInput" />
-      <SearchBtn>검색</SearchBtn>
+      <Input styleType="searchInput" placeholder="검색하실 제목을 입력하세요." value={searchValue} onChange={onSerachValue} />
+      <SearchBtn onClick={repo}>검색</SearchBtn>
     </FormContainer>
   );
 };
@@ -14,7 +26,7 @@ const SearchForm = (): JSX.Element => {
 const FormContainer = styled.div`
   display: flex;
   justify-content: space-between;
-  margin-bottom: 30px;
+  margin-bottom: 15px;
 `;
 
 const SearchBtn = styled.button`
