0c68e898f899a377fed996c6e6ad029ba1d06c5a
Woogie-94 <keu1106@naver.com>
2021-10-03 05:15:28 +0900

    Add feature of saving repo that favorite

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -1,16 +1,49 @@
-import React, { memo } from "react";
+import React, { memo, useCallback, useEffect } from "react";
 import styled from "styled-components";
-import { BsStar } from "react-icons/bs";
+import { BsStar, BsStarFill } from "react-icons/bs";
 import { VscRepoForked } from "react-icons/vsc";
 import { Repo } from "../types";
+import { useDispatch, useSelector } from "react-redux";
+import { addRepo, removeRepo, favoriteSelector, getState } from "../reducer/favorite";
 
 // use for containers SearchList and FavoriteRepo
 const RepoCard = ({ id, full_name, description, stargazers_count, forks_count, language, html_url }: Repo): JSX.Element => {
+  const favoriteRepos: Repo[] = useSelector(favoriteSelector);
+  const dispatch = useDispatch();
+
+  const isDuplicate = useCallback(() => {
+    let isDuplicate = false;
+
+    favoriteRepos.forEach((repo: Repo) => {
+      if (repo.id === id) isDuplicate = true;
+    });
+
+    return isDuplicate;
+  }, [favoriteRepos, id]);
+
+  const onAddRepo = useCallback(() => {
+    if (favoriteRepos.length >= 4) return;
+    if (isDuplicate()) return;
+
+    dispatch(addRepo({ id, full_name, description, stargazers_count, forks_count, language, html_url }));
+  }, [dispatch, isDuplicate, favoriteRepos, id, full_name, description, stargazers_count, forks_count, language, html_url]);
+
+  const onRemoveRepo = useCallback(() => {
+    dispatch(removeRepo({ id }));
+  }, [dispatch, id]);
+
+  useEffect(() => {
+    dispatch(getState(localStorage.getItem("favorite")));
+  }, [dispatch]);
+
   return (
     <CardContainer>
-      <CardTitle href={html_url} target="_blank">
-        {full_name}
-      </CardTitle>
+      <CardTop>
+        <CardTitle href={html_url} target="_blank">
+          {full_name}
+        </CardTitle>
+        {isDuplicate() ? <BsStarFill className="favorited" onClick={onRemoveRepo} /> : <BsStar onClick={onAddRepo} />}
+      </CardTop>
       <CardDesc>{description}</CardDesc>
       <CardEtc>
         {language && <CardEtcItem>{language}</CardEtcItem>}
@@ -39,7 +72,26 @@ const CardContainer = styled.div`
   border-radius: 4px;
 `;
 
+const CardTop = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  & > svg {
+    cursor: pointer;
+    font-size: 18px;
+    color: #aaa;
+
+    &.favorited {
+      color: #ffe16f;
+    }
+  }
+`;
+
 const CardTitle = styled.a`
+  overflow: hidden;
+  width: 90%;
+  padding-bottom: 3px;
+  text-overflow: ellipsis;
   text-decoration: underline;
   font-size: 20px;
 `;
