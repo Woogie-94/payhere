19dcb3347c384cc6f8d357796a47f4bbef665997
Woogie-94 <keu1106@naver.com>
2021-10-02 22:14:16 +0900

    Add infinite scroll

--- src/containers/SearchList.tsx
+++ src/containers/SearchList.tsx
@@ -1,20 +1,62 @@
-import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
+import React, { memo, useCallback, useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import RepoCard from "../components/RepoCard";
-import { ReposSelector } from "../reducer/repos";
+import { getRepos, ReposSelector } from "../reducer/repos";
 import { RepoReducerState, REQUEST_STATE } from "../types";
 import Loading from "../components/Loading";
+import useThrottle from "../hooks/useThrottle";
 
 const SearchList = (): JSX.Element => {
-  const { state, repos, empty }: RepoReducerState = useSelector(ReposSelector);
+  const [page, setPage] = useState<number>(1);
+  const scrollTarget = useRef<HTMLDivElement | null>(null);
+  const throttle = useThrottle();
+
+  const { state, repos, empty, searchValue }: RepoReducerState = useSelector(ReposSelector);
+  const dispatch = useDispatch();
+
+  const infiniteScroll = useCallback((): void => {
+    throttle(() => {
+      if (!scrollTarget.current) return;
+
+      const { clientHeight, scrollTop, scrollHeight } = scrollTarget.current;
+
+      if (clientHeight + scrollTop >= scrollHeight) {
+        // search api가 1000개까지만 검색을 허용해 임의로 제한을 둡니다 ㅠㅠ
+        // search api에 속도 제한이 있어 인증되지 않은 요청일 경우 분당 10번으로 제한되어 여러번 요청하면 에러 발생...
+        setPage((page) => (page < 90 ? page + 10 : page));
+      }
+    }, 200);
+  }, [throttle]);
+
+  useEffect(() => {
+    if (!searchValue) return;
+
+    if (page > 1) {
+      dispatch(getRepos({ searchValue, page }));
+    }
+  }, [page]);
+
+  useEffect(() => {
+    setPage(1);
+  }, [searchValue]);
 
   useEffect(() => {
     if (empty) alert("검색 결과가 없습니다.");
   }, [empty]);
 
+  useEffect(() => {
+    if (!scrollTarget.current) return;
+
+    scrollTarget.current.addEventListener("scroll", infiniteScroll);
+
+    return () => {
+      scrollTarget!.current!.removeEventListener("scroll", infiniteScroll);
+    };
+  }, [infiniteScroll]);
+
   return (
-    <ListContainer>
+    <ListContainer ref={scrollTarget}>
       {state === REQUEST_STATE.INIT && <FailedMessage>검색을 이용해보세요</FailedMessage>}
       {state === REQUEST_STATE.FAILED && <FailedMessage>잘못된 검색입니다.</FailedMessage>}
       {state === REQUEST_STATE.PENDING && <Loading />}
@@ -54,4 +96,4 @@ const FailedMessage = styled.div`
   color: #aaa;
 `;
 
-export default SearchList;
+export default memo(SearchList);
