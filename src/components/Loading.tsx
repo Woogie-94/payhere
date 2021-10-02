d613acd537956d632e8c7bbdef553eabfe684376
Woogie-94 <keu1106@naver.com>
2021-10-02 17:02:13 +0900

    Add use SearchList

---
+++ src/components/Loading.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+import ReactLoading from "react-loading";
+
+const Loading = (): JSX.Element => {
+  return (
+    <LoadingContainer>
+      <ReactLoading type="spin" color="#478bff" />
+    </LoadingContainer>
+  );
+};
+
+const LoadingContainer = styled.div`
+  position: absolute;
+  z-index: 10;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.5);
+`;
+
+export default Loading;
