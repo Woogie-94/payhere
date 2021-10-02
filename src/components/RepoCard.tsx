2f087110ad868e750e1adf4971304a11a95b1a49
Woogie-94 <keu1106@naver.com>
2021-10-02 17:03:34 +0900

    Add reactive to repo data

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -2,24 +2,28 @@ import React from "react";
 import styled from "styled-components";
 import { BsStar } from "react-icons/bs";
 import { VscRepoForked } from "react-icons/vsc";
+import { Repo } from "../types";
 
 // use for containers SearchList and FavoriteRepo
-const RepoCard = (): JSX.Element => {
+const RepoCard = ({ id, full_name, description, stargazers_count, forks_count, language, html_url }: Repo): JSX.Element => {
   return (
     <CardContainer>
-      <CardTitle>fullname</CardTitle>
-      <CardDesc>
-        이것저것 하는 공간입니다. 레포레포레포 이것저것 다양하게 많이 코딩 코딩다양하게 많이 코딩 코딩다양하게 많이 코딩 코딩다양하게 많이 코딩 코딩
-      </CardDesc>
+      <CardTitle href={html_url} target="_blank">
+        {full_name}
+      </CardTitle>
+      <CardDesc>{description}</CardDesc>
       <CardEtc>
-        <CardEtcItem>typescript</CardEtcItem>
-        <CardEtcItem>
-          <BsStar /> 5000
-        </CardEtcItem>
-        <CardEtcItem>
-          <VscRepoForked /> 40
-        </CardEtcItem>
-        <CardEtcItem>Updated 12days ago</CardEtcItem>
+        {language && <CardEtcItem>{language}</CardEtcItem>}
+        {stargazers_count > 0 && (
+          <CardEtcItem>
+            <BsStar /> {stargazers_count}
+          </CardEtcItem>
+        )}
+        {forks_count > 0 && (
+          <CardEtcItem>
+            <VscRepoForked /> {forks_count}
+          </CardEtcItem>
+        )}
       </CardEtc>
     </CardContainer>
   );
@@ -29,21 +33,23 @@ const CardContainer = styled.div`
   position: relative;
   width: 389px;
   height: 130px;
-  border: 1px solid #ddd;
-  border-radius: 4px;
   margin-bottom: 20px;
   padding: 15px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
 `;
 
-const CardTitle = styled.h3`
+const CardTitle = styled.a`
+  text-decoration: underline;
   font-size: 20px;
-  margin-bottom: 15px;
 `;
 
 const CardDesc = styled.p`
   overflow: hidden;
   width: 100%;
   padding: 2px 0;
+  margin-top: 15px;
+  color: #333;
   white-space: nowrap;
   text-overflow: ellipsis;
 `;
@@ -52,13 +58,17 @@ const CardEtc = styled.div`
   position: absolute;
   bottom: 15px;
   display: flex;
+
+  & > span:first-child {
+    margin-left: 0;
+  }
 `;
 
 const CardEtcItem = styled.span`
   display: flex;
-  margin-right: 10px;
+  margin-left: 10px;
   font-size: 14px;
-  color: #555;
+  color: #777;
 
   & > svg {
     margin-right: 3px;
