2db42a15c5fcd61dfadad575c7d3c4e10d63f32e
Woogie-94 <keu1106@naver.com>
2021-10-05 04:59:40 +0900

    Style side bar position standard

--- src/page/Favorite.tsx
+++ src/page/Favorite.tsx
@@ -1,14 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 import FavoriteList from "../containers/FavoriteList";
 import SearchSideBar from "../containers/SearchSideBar";
 
 const Favorite = (): JSX.Element => {
   return (
-    <>
+    <PositionStandard>
       <FavoriteList />
       <SearchSideBar />
-    </>
+    </PositionStandard>
   );
 };
+
+const PositionStandard = styled.div`
+  overflow: hidden;
+  position: relative;
+`;
 
 export default Favorite;
