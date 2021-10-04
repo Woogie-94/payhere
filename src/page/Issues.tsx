dc6b86e1f01db8a1353a6f89d4f55d4a6f3bb4db
Woogie-94 <keu1106@naver.com>
2021-10-05 03:35:58 +0900

    Add page Issues

---
+++ src/page/Issues.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import IssueList from "../containers/IssueList";
+
+const Issues = (): JSX.Element => {
+  return (
+    <>
+      <IssueList />
+    </>
+  );
+};
+
+export default Issues;
