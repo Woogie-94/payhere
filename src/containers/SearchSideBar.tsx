fd13c985adb0eb6a718ebc22315af5814e048e61
Woogie-94 <keu1106@naver.com>
2021-10-05 04:59:11 +0900

    Add SideBar open and close feature

--- src/containers/SearchSideBar.tsx
+++ src/containers/SearchSideBar.tsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
 import { ReposSelector } from "../reducer/repos";
 import { RepoReducerState, REQUEST_STATE } from "../types";
 import SearchForm from "./SearchForm";
 import SearchList from "./SearchList";
+import { HiChevronDoubleRight, HiChevronDoubleLeft } from "react-icons/hi";
 
 const SearchSideBar = (): JSX.Element => {
+  const [open, setOpen] = useState<boolean>(true);
   const { state, total_count }: RepoReducerState = useSelector(ReposSelector);
 
+  const onOpenAndClose = useCallback(() => {
+    setOpen((open) => !open);
+  }, []);
+
   return (
-    <BarContainer>
+    <BarContainer open={open}>
+      <OpenAndClose onClick={onOpenAndClose}>{open ? <HiChevronDoubleRight /> : <HiChevronDoubleLeft />}</OpenAndClose>
       <SearchForm />
       <TotalCount>검색 결과: {state === REQUEST_STATE.SUCCESS ? total_count : "-"}</TotalCount>
       <SearchList />
@@ -18,21 +25,31 @@ const SearchSideBar = (): JSX.Element => {
   );
 };
 
-const BarContainer = styled.div`
+const BarContainer = styled.div<{ open: boolean }>`
   position: absolute;
-  top: 80px;
-  right: 0;
+  top: 0;
+  right: ${({ open }) => (open ? "0" : "-450px")};
   width: 450px;
   height: calc(100vh - 80px);
   padding: 50px 30px;
   background-color: #fff;
   border-left: 1px solid #d6d6d8;
+
+  transition: 0.2s;
 `;
 
 const TotalCount = styled.div`
   font-size: 14px;
   color: #777;
   margin-bottom: 15px;
+`;
+
+const OpenAndClose = styled.div`
+  position: absolute;
+  top: 20px;
+  left: -30px;
+  font-size: 20px;
+  color: #333;
 `;
 
 export default SearchSideBar;
